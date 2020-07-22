9401d2fcd5998d246197a8717f6918ce501fb40d
rrabit42 <gegiraffe@gmail.com>
2020-07-22 12:59:33 +0900

    node.js - input from console

--- syntax/conditional.js
+++ syntax/conditional.js
@@ -1,6 +1,9 @@
+var args = process.argv;
+console.log(args) // 배열로 출력, [node.js runtime이 어디에 위치하는지, 우리가 실행시킨 이 파일에 대한 경로, 우리가 입력한 입력값], 즉 세번째부터 우리의 입력값들을 준다.
+console.log(args[2])
 console.log('A');
 console.log('B');
-if(false){
+if(args[2] === '1'){
   console.log('C1');
 }
 else {
