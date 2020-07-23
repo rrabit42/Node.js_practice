75eb725df8740c739995d94120a7cd508e5c7e15
rrabit42 <gegiraffe@gmail.com>
2020-07-24 00:04:46 +0900

    node.js - update function

--- main.js
+++ main.js
@@ -66,7 +66,7 @@ var app = http.createServer(function(request,response){
             var template = templateHTML(title, list, `
             <h2>${title}</h2>
             ${description}
-            `, `<a href="/create">create</a> <a href="/update?=${title}">update</a>`);
+            `, `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`);
             response.writeHead(200);
             response.end(template);
           });
@@ -78,16 +78,16 @@ var app = http.createServer(function(request,response){
         var title = 'WEB - create'
         var list = templateList(filelist);
         var template = templateHTML(title, list, `
-          <form action="http://localhost:3000/create_process" method="post">
-          <p><input type="text" name="title" placeholder="title"></p>
-          <p>
-            <textarea name="description" placeholder="description"></textarea>
-          </p>
-        
-          <p>
-            <input type="submit">
-          </p>
-        </form>
+          <form action="/create_process" method="post">
+            <p><input type="text" name="title" placeholder="title"></p>
+            <p>
+              <textarea name="description" placeholder="description"></textarea>
+            </p>
+          
+            <p>
+              <input type="submit">
+            </p>
+          </form>
         `, '');
         response.writeHead(200);
         response.end(template);
@@ -118,6 +118,49 @@ var app = http.createServer(function(request,response){
           response.writeHead(302,{Location: `/?id=${title}`});
           response.end();
         })
+      });
+    }
+    else if(pathname === '/update'){
+      fs.readdir('./data', function(error, filelist){
+        fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+          var title = queryData.id
+          var list = templateList(filelist);
+          var template = templateHTML(title, list, `
+            <form action="/update_process" method="post">
+              <input type="hidden" name="id" value="${title}">
+              
+              <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+              <p>
+                <textarea name="description" placeholder="description">${description}</textarea>
+              </p>
+            
+              <p>
+                <input type="submit">
+              </p>
+            </form>
+          `, `<a href="/create">create</a>`
+          );
+          response.writeHead(200);
+          response.end(template);
+        });
+      });
+    }
+    else if(pathname === '/update_process'){
+      var body = '';
+      request.on('data', function(data){
+        body += data;
+      });
+      request.on('end', function(){
+        var post = qs.parse(body); // 정보를 객체화
+        var id = post.id
+        var title = post.title;
+        var description = post.description;
+        fs.rename(`data/${id}`, `data/${title}`, function(error){
+          fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+            response.writeHead(302,{Location: `/?id=${title}`});
+            response.end();
+          });
+        });
       });
     }
     else {
