33684c31e9285a7d0e086a00c9e5ffb04a6d3c1b
rrabit42 <gegiraffe@gmail.com>
2020-07-27 02:07:09 +0900

    Express.js - create Middleware

--- main.js
+++ main.js
@@ -17,23 +17,29 @@ var compression = require('compression');
 */
 app.use(bodyParser.urlencoded({ extended: false }));
 // compression() 함수가 middleware를 리턴하고, app.use를 통해 미들웨어가 장착됨
-app.use(compression());
+app.use(compression()); // 요청데이터가 너무 많아서, zip으로 압축됨
+// app.use로 하면 모든 request에 filelist가 담겨있음 => 비효율
+// 따라서 아래와 같이 바꿈, get방식의 모든 요청('*')에 이 미들웨어가 적용되도록!
+app.get('*', function(request, response, next){
+  fs.readdir('./data', function(error, filelist){
+    request.list = filelist; // 모든 라우트 안에서 request의 list property를 통해 filelist에 접근 가능
+    next(); // next라는 변수에 그 다음에 호출되어야할 미들웨어가 담겨있음, 그럼 이 코드로 인해 실행이 되겠지!
+  })
+});
 
 // parse application/json
 // app.use(bodyParser.json())
 
 // route, routing : 기존의 node.js에서는 if문으로 처리함
 app.get('/', (request, response) => {
-  fs.readdir('./data', function(error, filelist){
-    var title = 'Welcome';
-    var description = 'Hello, Node.js';
-    var list = template.list(filelist);
-    var html = template.html(title, list, `
-            <h2>${title}</h2>
-            ${description}
-          `, `<a href="/create">create</a>`);
-    response.send(html)
-  })
+  var title = 'Welcome';
+  var description = 'Hello, Node.js';
+  var list = template.list(request.list);
+  var html = template.html(title, list, `
+          <h2>${title}</h2>
+          ${description}
+        `, `<a href="/create">create</a>`);
+  response.send(html)
 }) // (path, callback[, callback...])
 
 /*
@@ -44,50 +50,44 @@ Request URL: http://localhost:3000/users/34/books/8989
 req.params: { "userId": "34", "bookId": "8989" }
 */
 app.get('/page/:pageId', (request, response) => {
-  fs.readdir('./data', function(error, filelist){
-    var filteredId = path.parse(request.params.pageId).base;
-
-    fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
-      var title = request.params.pageId
-      var sanitizedTitle = sanitizeHtml(title);
-      var sanitizeDescription = sanitizeHtml(description, {
-        allowedTags: ['h1']
-      });
-      var list = template.list(filelist);
-
-      var html = template.html(sanitizedTitle, list, `
-      <h2>${sanitizedTitle}</h2>
-      ${sanitizeDescription}`,
-      ` <a href="/create">create</a>
-        <a href="/update/${sanitizedTitle}">update</a>
-        <form action="/delete_process" method="post" onsubmit="">
-          <input type="hidden" name="id" value="${sanitizedTitle}">
-          <input type="submit" value="delete">
-        </form>
-      `);
-      response.send(html);
+  var filteredId = path.parse(request.params.pageId).base;
+  fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
+    var title = request.params.pageId
+    var sanitizedTitle = sanitizeHtml(title);
+    var sanitizeDescription = sanitizeHtml(description, {
+      allowedTags: ['h1']
     });
+    var list = template.list(request.list);
+    var html = template.html(sanitizedTitle, list, `
+    <h2>${sanitizedTitle}</h2>
+    ${sanitizeDescription}`,
+    ` <a href="/create">create</a>
+      <a href="/update/${sanitizedTitle}">update</a>
+      <form action="/delete_process" method="post" onsubmit="">
+        <input type="hidden" name="id" value="${sanitizedTitle}">
+        <input type="submit" value="delete">
+      </form>
+    `);
+    response.send(html);
   });
 })
 
 app.get('/create', (request, response) => {
-  fs.readdir('./data', function(error, filelist){
-    var title = 'WEB - create'
-    var list = template.list(filelist);
-    var html = template.html(title, list, `
-      <form action="/create_process" method="post">
-        <p><input type="text" name="title" placeholder="title"></p>
-        <p>
-          <textarea name="description" placeholder="description"></textarea>
-        </p>
-      
-        <p>
-          <input type="submit">
-        </p>
-      </form>
-    `, '');
-    response.send(html);
-  });
+  var title = 'WEB - create'
+  var list = template.list(request.list);
+  var html = template.html(title, list, `
+    <form action="/create_process" method="post">
+      <p><input type="text" name="title" placeholder="title"></p>
+      <p>
+        <textarea name="description" placeholder="description"></textarea>
+      </p>
+    
+      <p>
+        <input type="submit">
+      </p>
+    </form>
+  `, '');
+  response.send(html);
 })
 
 //form에서 action="/create" method="post"로 하고
@@ -116,28 +116,26 @@ app.post('/create_process', (request, response) => {
 })
 
 app.get('/update/:pageId', (request, response) => {
-  fs.readdir('./data', function(error, filelist){
-    var filteredId = path.parse(request.params.pageId).base;
-    fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
-      var title = request.params.pageId
-      var list = template.list(filelist);
-      var html = template.html(title, list, `
-        <form action="/update_process" method="post">
-          <input type="hidden" name="id" value="${title}">
-          
-          <p><input type="text" name="title" placeholder="title" value="${title}"></p>
-          <p>
-            <textarea name="description" placeholder="description">${description}</textarea>
-          </p>
+  var filteredId = path.parse(request.params.pageId).base;
+  fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
+    var title = request.params.pageId
+    var list = template.list(request.list);
+    var html = template.html(title, list, `
+      <form action="/update_process" method="post">
+        <input type="hidden" name="id" value="${title}">
         
-          <p>
-            <input type="submit">
-          </p>
-        </form>
-      `, `<a href="/create">create</a>`
-      );
-      response.send(html);
-    });
+        <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+        <p>
+          <textarea name="description" placeholder="description">${description}</textarea>
+        </p>
+      
+        <p>
+          <input type="submit">
+        </p>
+      </form>
+    `, `<a href="/create">create</a>`
+    );
+    response.send(html);
   });
 })
 
