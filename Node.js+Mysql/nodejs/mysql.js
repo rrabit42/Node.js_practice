cbac10690cd0bbc8fe91cd75f168b7ab2c7ba3e0
rrabit42 <gegiraffe@gmail.com>
2020-07-29 16:35:32 +0900

    Node + Mysql - mysql module & connect

---
+++ Node.js+Mysql/nodejs/mysql.js
@@ -0,0 +1,21 @@
+// mysql 모듈 사용
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost', // database서버가 어떤 컴퓨터에 있는가, mysql과 node가 같은 서버에 있으면 localhost
+  user     : 'me',
+  password : 'secret',
+  database : 'my_db'
+});
+
+connection.connect();
+
+// (sql문, callback)
+// sql문 데이터베이스 서버에 전송돼서 실행이 끝난 다음에 콜백이 호출될 것
+connection.query('SELECT * FROM topic', function(error, results, fields){
+  if(error) {
+    console.log(error);
+  }
+  console.log('The solution is: ', results);
+});
+
+connection.end();
